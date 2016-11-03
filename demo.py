Normalize gradient, fix backpropagation.

demo.py
@@ -6,7 +6,7 @@ x = np.array([
 	[ 1.0,  1.0],
 	[ 1.0, -1.0],
 	[-1.0,  1.0],
-	[-1.0, -1.0],
+	[-1.0, -1.0]
 ])
 y = np.array([
 	[ 1.0, 0.0],
@@ -19,6 +19,5 @@ model = nnet.Model([
 	nnet.layers.ReLU(2, 10),
 	nnet.layers.Softmax(10, 2)
 ], loss='xentropy')
-print(model.predict(x))
-print(model.fit(x, y))
+model.fit(x, y, epochs=100)
 print(model.predict(x))

nnet/layers/relu.py
@@ -22,9 +22,9 @@ class ReLU:
 		return self._relu(y)
 
 	def backpropagate(self, grad):
-		ngrad = np.zeros((self.input_dims))
+		ngrad = []
 		for i, x_i in enumerate(self.data):
 			partial = grad[i] * self._grad_relu(np.dot(self.weight, x_i))
 			self.weight += 1e-3 * np.outer(partial, x_i)
-			ngrad += np.dot(partial, self.weight)
-		return ngrad
+			ngrad.append(np.dot(partial, self.weight))
+		return np.array(ngrad)

nnet/layers/softmax.py
@@ -9,15 +9,13 @@ class Softmax:
 	def _softmax(self, y):
 		result = []
 		for i, y_i in enumerate(y):
-			e_y_i = np.exp(y_i)
+			e_y_i = np.exp(y_i - np.max(y_i))
 			result.append(e_y_i / np.sum(e_y_i))
 		return np.array(result)
 
 	def _grad_softmax(self, y):
 		# TODO: Actually compute the gradient...
 		def approx(y_i):
-			if 0.33 < y_i < 0.66:
-				return 1.0
 			return 0.1
 		approx = np.vectorize(approx)
 		return approx(y)
@@ -32,9 +30,9 @@ class Softmax:
 		return self._softmax(y)
 
 	def backpropagate(self, grad):
-		ngrad = np.zeros((self.input_dims))
+		ngrad = []
 		for i, x_i in enumerate(self.data):
 			partial = grad[i] * self._grad_softmax(np.dot(self.weight, x_i))
 			self.weight += np.outer(partial, x_i)
-			ngrad += np.dot(partial, self.weight)
-		return ngrad
+			ngrad.append(np.dot(partial, self.weight))
+		return np.array(ngrad)

nnet/model.py
@@ -4,13 +4,13 @@ def mse(expected, output):
 	assert expected.shape == output.shape
 	gradient = expected - output
 	loss = np.sum(np.power(expected - output, 2))
-	return (loss, gradient)
+	return (loss / expected.shape[0], gradient / expected.shape[0])
 
 def xentropy(expected, output):
 	assert expected.shape == output.shape
 	gradient = expected / output
 	loss = -np.sum(expected * np.log(output))
-	return (loss, gradient)
+	return (loss / expected.shape[0], gradient / expected.shape[0])
 
 class Model:
 	def __init__(self, layers, loss='mse'):